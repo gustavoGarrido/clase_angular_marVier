50dae914efee75c8718063c59846fc8504691602
Gustavo G <ingindustrial.gustavo@gmail.com>
2021-05-15 12:24:57 -0300

    bindind de datos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'claseMarVier';
+  binding = "Hola mundo bindind"
+  nombre:string = "Gustavo"
+
+  clase_prueba = "color_red"
+  cambio_etiqueta = "etiqueta vieja"
+  clase_titulo = "black"
+
+  cambiarEtiqueta(){
+    this.cambio_etiqueta = "etiqueta nueva"
+  }
+
+  cambiarColor(color:string){
+    this.clase_titulo = color
+    console.log(color)
+  }
+
 }
