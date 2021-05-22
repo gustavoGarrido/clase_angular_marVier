95a0a51e221e1208e4a6ab833ad9f158747386c3
Gustavo G <ingindustrial.gustavo@gmail.com>
2021-05-22 10:03:13 -0300

    clase 1 angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { ComponentePruebaComponent } from './componente-prueba/componente-prueba.component';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'claseMarVier';
+
+  constructor(){}
+
+  nombre_aplicacion:string = "App curso Martes y viernes";
+
+  mostrar_clase:string = "propuedad1"
+
+  nombre:string = "nombre"
+
+  colorTitulo:string = "p1"
+
+  nombreColor:string = ""
+
+  cambiarClase(){
+    this.mostrar_clase = "propuedad2"
+  }
+
+  cambiarColorTitulo(color:string){
+    this.colorTitulo = color
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { ComponentePruebaComponent } from './componente-prueba/componente-prueba.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentePruebaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
