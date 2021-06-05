dffb1219b15370d4768767a98973ee497e27643a
Gustavo G <ingindustrial.gustavo@gmail.com>
2021-06-04 21:26:04 -0300

    Creado formularios reactivos

--- src/app/pages/usuarios/usuarios.component.ts
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -11,25 +11,33 @@ export class UsuariosComponent implements OnInit {
   constructor(public fb:FormBuilder) { }
 
   formCrearUsuario= this.fb.group({
-    nombre:["",Validators.required],
-    apellido:[],
-    tipo_doc:[],
-    numero_doc:[],
-    email:[],
-    telefono:[],
-    edad:[],
-    nombre_tutor:[],
-    password:[]
+    nombre:["Gustavo",[Validators.required, Validators.pattern(/^([A-Z]|[a-z])+$/)]],
+    apellido:["", Validators.required],
+    tipo_doc:["", Validators.required],
+    numero_doc:["", [Validators.required, Validators.maxLength(8), Validators.minLength(6)]],
+    email:["", [Validators.required, Validators.email]],
+    telefono:["", Validators.pattern(/^(15|11|\+5411|\+5415|5415|5411)?[2-9]\d{7}$/)],
+    edad:["", [Validators.required, Validators.max(100)]],
+    nombre_tutor:["", Validators.required],
+    password:["", Validators.required]
   })
 
   registro(){
-    if(this.formCrearUsuario.valid == false){
-      //mostrar cartelito con color rojo
+    console.log(this.formCrearUsuario.valid)
+    if(this.formCrearUsuario.valid){
+      //Implementar un servicio que envia los datos al backend
+    }
+  }
+
+  validarEdad(){
+    if(this.formCrearUsuario.get('edad')?.value > 18){
+      this.formCrearUsuario.get('nombre_tutor')?.clearValidators();
+      this.formCrearUsuario.get('nombre_tutor')?.updateValueAndValidity();
     }
     else{
-      //registra en base de datos
+      this.formCrearUsuario.get('nombre_tutor')?.setValidators(Validators.required)
+      this.formCrearUsuario.get('nombre_tutor')?.updateValueAndValidity();
     }
-    console.log(this.formCrearUsuario.valid)
   }
 
   ngOnInit(): void {
